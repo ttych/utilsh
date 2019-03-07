719205664e6366ad5366844764708b74b52e5bda
ttych <thomas.tych@gmail.com>
2019-03-07 19:12:12 +0100

    [build] update build process

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ install_utilsh()
 {
     printf "Cloning the utilsh's GitHub repository...\n"
     git clone "$UTILSH_URL" "$UTILSH_INSTALL_DIR" &&
+        cd "$UTILSH_INSTALL_DIR" &&
+        git submodule update --init &&
         "$UTILSH_INSTALL_DIR"/bin/utilsh update
 }
 
