b927ecbe440625faceb30ec8a78693a69b95ef4c
ttych <thomas.tych@gmail.com>
2019-03-08 10:19:06 +0100

    [build] fix link filtering

--- install.sh
+++ install.sh
@@ -17,7 +17,18 @@ install_utilsh
 install_full()
 {
     UTILSH_DIR="$UTILSH_INSTALL_DIR"
+
+    "$UTILSH_DIR"/bin/utilsh source add https://github.com/ttych/sh-libs.git
+    "$UTILSH_DIR"/bin/utilsh source add https://github.com/ttych/goto.git
     "$UTILSH_DIR"/bin/utilsh source add https://github.com/ttych/pyv.git
+    "$UTILSH_DIR"/bin/utilsh source add https://github.com/ttych/wtmux.git
+    "$UTILSH_DIR"/bin/utilsh source add https://github.com/ttych/wwine.git
+
+    "$UTILSH_DIR"/bin/utilsh autoload std.shl
+    "$UTILSH_DIR"/bin/utilsh autoload local.shl
+    "$UTILSH_DIR"/bin/utilsh autoload git_scm.shl
+    "$UTILSH_DIR"/bin/utilsh autoload goto.shl
+    "$UTILSH_DIR"/bin/utilsh autoload ruby_base.shl
     "$UTILSH_DIR"/bin/utilsh autoload pyv.sh
 }
 
